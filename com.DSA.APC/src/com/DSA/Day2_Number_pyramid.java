37b55ec9be7268e6358f5cd37f3b8479a87f1def
pradeeptaware6269 <pradeeptaware6269@gmail.com>
2026-04-15 17:47:10 +0530

    Advanced Pattern

---
+++ com.DSA.APC/src/com/DSA/Day2_Number_pyramid.java
@@ -0,0 +1,20 @@
+package com.DSA;
+
+public class Day2_Number_pyramid {
+
+	public static void main(String[] args) {
+		int n = 5;
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 4; j >= i; j--) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i; k++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
---
+++ com.DSA.APC/src/com/DSA/Day2_Palindromic_Pattern.java
@@ -0,0 +1,25 @@
+package com.DSA;
+
+public class Day2_Palindromic_Pattern {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= (n - i); j++) {
+				System.out.print(" ");
+			}
+			for (int j = i; j >= 1; j--) {
+				System.out.print(j);
+			}
+			for (int j = 1; j <= i; j++) {
+				if (j == 1) {
+					continue;
+				}
+				System.out.print(j);
+			}
+
+			System.out.println();
+		}
+
+	}
+
+}
